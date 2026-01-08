04022e2708c7613e9879ed00027aa7e5ffa83133
abspandana4321-jpg <abspandana4321@gmail.com>
2026-01-08 14:43:36 +0530

    Add files via upload

---
+++ stringtonumberconversion.py
@@ -0,0 +1,11 @@
+
+
+price = input("Enter the price of the item: ")
+quantity = input("Enter the quantity: ")
+
+price = float(price)
+quantity = int(quantity)
+
+total_cost = price * quantity
+
+print("Total Cost:", total_cost)
